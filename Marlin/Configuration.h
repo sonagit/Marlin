3eb173d34b2ede5000d5c6975586a206b6fb0a00
Stu <supadupastu@gmail.com>
2017-08-08 11:29:14 -0700

    extruder steps/mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -487,7 +487,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 731 } // robostu
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 799 } // robostu
 
 /**
  * Default Max Feed Rate (mm/s)
